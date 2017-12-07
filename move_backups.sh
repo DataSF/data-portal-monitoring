f97f7d154113108d7fbc9814be38aeb80ae4f209
j9recurses <janine.heiser@gmail.com>
2017-12-07 15:46:21 -0800

    adds bash scripts for automated backups

---
+++ move_backups.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+while [ $# -gt 0 ]; do
+        case $1 in
+                -c)
+                        CONFIG_FILE_PATH="$2"
+                        shift 2
+                        ;;
+                *)
+                        ${ECHO} "Unknown Option \"$1\"" 1>&2
+                        exit 2
+                        ;;
+        esac
+done
+ 
+if [ -z $CONFIG_FILE_PATH ] ; then
+        SCRIPTPATH=$(cd ${0%/*} && pwd -P)
+        CONFIG_FILE_PATH="${SCRIPTPATH}/pg_backup.config"
+fi
+ 
+if [ ! -r ${CONFIG_FILE_PATH} ] ; then
+        echo "Could not load config file from ${CONFIG_FILE_PATH}" 1>&2
+        exit 1
+fi
+ 
+source "${CONFIG_FILE_PATH}"
+SUFFIX="-daily"
+FINAL_BACKUP_DIR=$BACKUP_DIR"`date +\%Y-\%m-\%d`$SUFFIX/"
+echo $FINAL_BACKUP_DIR
+SUFFIX2=$SUFFIX".tar.gz"
+ARCHIVE="`date +\%Y-\%m-\%d`$SUFFIX2"
+tar -zcvf $BACKUP_DIR$ARCHIVE $FINAL_BACKUP_DIR
+scp -i /home/j9/.ssh/id_rsa $BACKUP_DIR$ARCHIVE "pgbackups@162.243.137.94:/home/pgbackups/data_monitoring_backups"
